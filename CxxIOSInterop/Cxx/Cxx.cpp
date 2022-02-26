54d3ab54192aa5869c22bb8921bc93184d71a89b
Caleb Meurer <cabmeurer@gmail.com>
2022-02-26 12:07:07 -0700

    Create ios app to use cxx_interop

---
+++ CxxIOSInterop/Cxx/Cxx.cpp
@@ -0,0 +1,10 @@
+//
+//  Cxx.cpp
+//  CxxIOSInterop
+//
+//  Created by Caleb Meurer on 2/25/22.
+//
+
+int cxx_function(int n) {
+    return n;
+}
---
+++ CxxIOSInterop/Cxx/Cxx.hpp
@@ -0,0 +1,11 @@
+//
+//  Cxx.hpp
+//  CxxIOSInterop
+//
+//  Created by Caleb Meurer on 2/25/22.
+//
+
+#ifndef Cxx_hpp
+#define Cxx_hpp
+int cxx_function(int n);
+#endif
